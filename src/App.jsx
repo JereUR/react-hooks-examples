fa5175f4ef33a8af95f65f0c248c306b1453d7ab
JereUR <jeremias.jdv@gmail.com>
2023-10-19 22:34:04 -0300

    feat(Root): useMemo, useRef and useDocumentTitle custom hook added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { createContext } from 'react'
 import './App.css'
 import ComponentA from './components/HookUseContext/ComponentA'
 import HookUseEffect from './components/HookUseEffect/HookUseEffect'
-import HookUseState from './components/HookUseState'
+import HookUseState from './components/HookUseState/HookUseState'
 import HookUseReducer from './components/HookUseReducer/HookUseReducer'
 import HookUseCallback from './components/HookUseCallback/HookUseCallback'
+import HookUseMemo from './components/HookUseMemo/HookUseMemo'
+import HookUseRef from './components/HookUseRef/HookUseRef'
+import CustomHooks from './components/CustomHooks/CustomHooks'
 
 export const UserContext = createContext()
 export const ChannelContext = createContext()
@@ -22,6 +25,9 @@ function App() {
       </UserContext.Provider>
       <HookUseReducer />
       <HookUseCallback />
+      <HookUseMemo />
+      <HookUseRef />
+      <CustomHooks />
     </>
   )
 }
