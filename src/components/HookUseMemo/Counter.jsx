76d6e625cfb7a8c45902efa91af39ccb9c233bf1
JereUR <jeremias.jdv@gmail.com>
2023-10-19 21:52:16 -0300

    feat(Root): UseMemo hook components

---
+++ src/components/HookUseMemo/Counter.jsx
@@ -0,0 +1,34 @@
+import { useMemo, useState } from 'react'
+
+export default function Counter() {
+  const [counterOne, setCounterOne] = useState(0)
+  const [counterTwo, setCounterTwo] = useState(0)
+
+  const incrementOne = () => {
+    setCounterOne(counterOne + 1)
+  }
+
+  const incrementTwo = () => {
+    setCounterTwo(counterTwo + 1)
+  }
+
+  const isEvenWithDelay = useMemo(() => {
+    let i = 0
+    //Delay
+    while (i < 2000000000) i++
+
+    return counterOne % 2 === 0
+  }, [counterOne])
+
+  return (
+    <div>
+      <div>
+        <button onClick={incrementOne}>Count One - {counterOne}</button>
+        <span>{isEvenWithDelay ? 'Even' : 'Odd'}</span>
+      </div>
+      <div>
+        <button onClick={incrementTwo}>Count Two - {counterTwo}</button>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/HookUseMemo/HookUseMemo.jsx
@@ -0,0 +1,12 @@
+import Counter from './Counter'
+
+export default function HookUseMemo() {
+  return (
+    <div>
+      <h1>UseMemo Hook</h1>
+      <Counter />
+      <br />
+      <hr />
+    </div>
+  )
+}
